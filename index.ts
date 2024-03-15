847f9e778d37ac9acb91a9be19bf3a599089cc05
umakossiooo <a01254619@tec.mx>
2024-03-15 02:13:41 -0700

    new features commit

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
-import express, {Express, Request, Response} from 'express';
-import apiRouter from './src/routes';
+import express, { Express, Request, Response } from 'express';
 const morgan = require('morgan');
+import apiRouter from './src/routes';
+const db = require('./src/models')
 
-const app: Express= express();
+const app: Express = express();
 const port = 3000;
+
+db.sequelize.sync()
+.then(() => {
+console.log("Synced db.");
+})
+.catch((err:Error) => {
+console.error("Failed to sync db: " , err.message);
+});
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(apiRouter);
 
-app.listen(port, () =>
-{
+app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+    });
