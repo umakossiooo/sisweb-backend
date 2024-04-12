3b07977a1ae90983d90a51e0f963ac031a527358
umakossiooo <a01254619@tec.mx>
2024-04-12 09:44:54 -0700

    feat connection to front end

--- index.ts
+++ index.ts
@@ -2,12 +2,13 @@ import express, { Express } from "express";
 import apiRouter from './src/routes';
 import connectionDB from './src/connection/connection';
 const morgan = require('morgan');
+const cors = require('cors');
 
 
 const app: Express = express();
 const port = 3000;
 
-
+app.use(cors({ origin: true }));
 app.use(morgan('dev'));
 app.use(express.json()); 
 app.use(apiRouter);
